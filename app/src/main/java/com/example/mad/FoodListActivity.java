eea5a66b9449c8b99760e32ef3ff3e0fdd2f33f5
Randula Mawaththta <randula98@gmail.com>
2022-10-30 10:19:00 +0530

    Implement

--- app/src/main/java/com/example/mad/FoodListActivity.java
+++ app/src/main/java/com/example/mad/FoodListActivity.java
@@ -1,14 +1,33 @@
 package com.example.mad;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FoodListActivity extends AppCompatActivity {
+
+    private Button btnPizza;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foodl_ist);
+
+        btnPizza = (Button) findViewById(R.id.btnPizza);
+        btnPizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openItemsActivity();
+            }
+        });
+    }
+
+    public void openItemsActivity(){
+        Intent intent = new Intent(this , CartActivity.class);
+        startActivity(intent);
     }
 }
